0de867b139d97f26180789f290049c46da5e0e1f
Aleksander Karmanov <karmanov.aleks2016@yandex.ru>
2026-08-26 08:30:54 +0300

    Исправлены замечания Ruff и подключен запуск синхронизации

--- src/events_aggregator/models/__init__.py
+++ src/events_aggregator/models/__init__.py
@@ -2,3 +2,10 @@ from .event import Event
 from .place import Place
 from .syncmetadata import SyncMetadata
 from .tickets import Ticket
+
+__all__ = [
+    "Event",
+    "Place",
+    "SyncMetadata",
+    "Ticket",
+]
